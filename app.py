b3aebeeb8b5cafd5b568fbdfeee16ee34f4408e7
Derick Sayavong <dsayavong54651@gmail.com>
2024-11-17 23:48:45 -0500

    Added enzyme type to restriction sites

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sbol_utilities import component
 from sbol_utilities.component import ed_restriction_enzyme, backbone, part_in_backbone,  part_in_backbone_from_sbol, \
     digestion, ligation, Assembly_plan_composite_in_backbone_single_enzyme, backbone_from_sbol
 from sbol_utilities.conversion import convert_from_genbank, convert3to2
+from Bio import Restriction
 
 
 app = Flask(__name__)
@@ -21,6 +22,8 @@ def getSBOL():
         # Vector and Insert and sequences
         vector_data = request_data['Vector']
         insert_data = request_data['Insert']
+
+        # The data will be Enzyme name
         first_restriction_site_data = request_data['First Restriction Site']
         second_restriction_site_data = request_data['Second Restriction Site']
 
@@ -41,28 +44,53 @@ def getSBOL():
 
     return jsonify(SBOL_data)  # Flask will convert this to JSON
 
-def create_vector(doc, data):
+def create_vector(doc, sequence):
     vector = sbol3.Component("Vector", sbol3.SBO_DNA)
     doc.add(vector)
-    vector_sequence = sbol3.Sequence("vectorSequence", elements=data, encoding=sbol3.IUPAC_DNA_ENCODING)
+    vector_sequence = sbol3.Sequence("vectorSequence", elements=sequence, encoding=sbol3.IUPAC_DNA_ENCODING)
     doc.add(vector_sequence)
 
-def create_insert(doc, data):
+def create_insert(doc, sequence):
     insert = sbol3.Component("Insert", sbol3.SBO_DNA)
     doc.add(insert)
-    insert_sequence = sbol3.Sequence("insert_sequence", elements=data, encoding=sbol3.IUPAC_DNA_ENCODING)
+    insert_sequence = sbol3.Sequence("insert_sequence", elements=sequence, encoding=sbol3.IUPAC_DNA_ENCODING)
     doc.add(insert_sequence)
 
-def create_first_restriction_site(doc, data):
-    first_restriction_site_enzyme = sbol3.Component("first_restriction_enzyme", sbol3.SBO_BIOCHEMICAL_REACTION)
-    doc.add(first_restriction_site_enzyme)
-    enzyme_sequence = sbol3.Sequence("first_restriction_site_enzyme", elements=data, encoding=sbol3.IUPAC_DNA_ENCODING)
+def create_first_restriction_site(doc, enzyme_name):
+    all_enzymes = Restriction.AllEnzymes
+    for enzyme in all_enzymes:
+        if enzyme.__name__ == enzyme_name:
+            print(f"Name: {enzyme_name}, URI: {enzyme.uri}")
+        
+    # enzyme = ed_restriction_enzyme(enzyme_name)
+    definition = f'http://rebase.neb.com/rebase/enz/{enzyme_name}.html'
+    enzyme = sbol3.ExternallyDefined(["SBO:0000176"], definition=enzyme.uri, name=enzyme_name)
+    doc.add(enzyme)
+
+    #! GOOD
+    # enzyme_type = "SBO_0000014"
+
+    # first_restriction_site_enzyme = sbol3.Component("first_restriction_enzyme", enzyme_type, type_uri=enzyme.uri)
+    # doc.add(first_restriction_site_enzyme)
+
+    enzyme_sequence = sbol3.Sequence("first_restriction_site_enzyme", elements="TEST", encoding=sbol3.IUPAC_DNA_ENCODING)
     doc.add(enzyme_sequence)
 
-def create_second_restriction_site(doc, data):
-    second_restriction_site_enzyme = sbol3.Component("second_restriction_enzyme", sbol3.SBO_BIOCHEMICAL_REACTION)
+def create_second_restriction_site(doc, enzyme_name):
+    all_enzymes = Restriction.AllEnzymes
+    for enzyme in all_enzymes:
+        if enzyme.__name__ == enzyme_name:
+            print(f"Name: {enzyme_name}, URI: {enzyme.uri}")
+
+    # enzyme = ed_restriction_enzyme(enzyme_name)
+    # doc.add(enzyme)
+
+    enzyme_type = "SBO_0000014"
+
+    second_restriction_site_enzyme = sbol3.Component("second_restriction_enzyme", enzyme_type, type_uri=enzyme.uri)
     doc.add(second_restriction_site_enzyme)
-    enzyme_sequence = sbol3.Sequence("second_restriction_site_enzyme", elements=data, encoding=sbol3.IUPAC_DNA_ENCODING)
+
+    enzyme_sequence = sbol3.Sequence("second_restriction_site_enzyme", elements="TEST", encoding=sbol3.IUPAC_DNA_ENCODING)
     doc.add(enzyme_sequence)
 
         
